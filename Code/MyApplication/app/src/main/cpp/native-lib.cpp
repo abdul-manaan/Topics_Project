c6fca95ae68bfc2108c7348a6a114ffb72bb5d74
abdul-manaan <manan.lums@gmail.com>
2019-03-04 19:28:26 +0500

    App that creates memory pressure

---
+++ Code/MyApplication/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,107 @@
+#include <jni.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <dirent.h>
+#include <unistd.h>
+#include <android/log.h>
+#include <iostream>
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+using namespace std;
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_myapplication_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject instance,
+        jint oom,
+        jint mem,
+        string fname) {
+    std::string hello = "Hello C++";
+    setuid(0);
+    nice(oom);
+    char *process_name="org.chromium.chrome";
+    int id;
+    pid_t pid = -1;
+    DIR* dir;
+    FILE *fp;
+    char filename[32];
+    char cmdline[256];
+
+    struct dirent * entry;
+
+
+    if (process_name == NULL)
+        pid= -1;
+
+    dir = opendir("/proc");
+    if (dir == NULL)
+        pid= -1;
+
+    while((entry = readdir(dir)) != NULL) {
+        id = atoi(entry->d_name);
+        if (id != 0) {
+            sprintf(filename, "/proc/%d/cmdline", id);
+            fp = fopen(filename, "r");
+            if (fp) {
+                fgets(cmdline, sizeof(cmdline), fp);
+                fclose(fp);
+
+                if (strcmp(process_name, cmdline) == 0) {
+                    /* process found */
+                    pid = id;
+                    break;
+                }
+            }
+        }
+    }
+
+    closedir(dir);
+
+
+
+    vector<string> s;
+
+    FILE* file;
+    file = fopen((std::string("/sdcard/")+fname).c_str(), "wb");
+
+    if (file != NULL)
+    {
+
+        for(int i=0;i<mem;i++) {
+            string sstr(1024*1024, ' ');
+            s.push_back(sstr);
+            int tSize = 0, resident = 0, share = 0;
+            int tSize1 = 0, resident1 = 0, share1 = 0;
+            ifstream buffer("/proc/self/statm");
+            buffer >> tSize >> resident >> share;
+            buffer.close();
+            const char * pid_char=to_string(id).c_str();
+            char proc_chrome_dir[1024]="/proc/";
+            strcat(proc_chrome_dir,pid_char);
+            strcat(proc_chrome_dir,"/statm");
+            ifstream buffer1(proc_chrome_dir);
+            buffer1 >> tSize1 >> resident1 >> share1;
+            buffer1.close();
+
+            long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+            double self_rss = resident * page_size_kb;
+
+            double self_shared_mem = share * page_size_kb;
+
+            double chrome_rss = resident1 * page_size_kb;
+
+            double chrome_shared_mem = share1 * page_size_kb;
+
+            __android_log_print(ANDROID_LOG_ERROR, "TRACKERS", "index: %d \t Self_PSS: %f \t Chrome_PSS: %f",i,(self_rss - self_shared_mem), (chrome_rss - chrome_shared_mem));
+            fprintf(file, "%d,%f,%f\n",i,(self_rss - self_shared_mem), (chrome_rss - chrome_shared_mem));
+        }
+        fflush(file);
+        fclose(file);
+
+    } else {
+        __android_log_print(ANDROID_LOG_ERROR, "TRACKERS", "File error! Can't Open File");
+
+    }
+
+    return env->NewStringUTF(hello.c_str());
+}
---
+++ Code/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.out;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String []{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);        }
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI(int oom,int mem,String fname);
+
+    public void callMe(View view) {
+        out.println("LOA");
+        EditText _fname = findViewById(R.id.fileName);
+        EditText _oom = findViewById(R.id.oomAdj);
+        EditText _mem = findViewById(R.id.memoryLimit);
+        String fname = _fname.getText().toString();
+        String oom = _oom.getText().toString();
+        String mem = _mem.getText().toString();
+        out.printf("%s %d %s",fname, parseInt(oom), parseInt(mem));
+        out.println(stringFromJNI( parseInt(oom), parseInt(mem), fname));
+
+    }
+}
